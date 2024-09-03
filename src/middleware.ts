40125efc91e808e8cb16f60c5722359f942d8294
Himanshu Kumar <himanshu064@gmail.com>
2024-09-03 11:14:27 +0530

    Configured the static images to not to be included in the middleware check

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,5 +27,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|assets|_next/static|_next/image|favicon.ico).*)"],
 };
