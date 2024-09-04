524e030dc1811ff2fda4f6b47b355003779136e7
Himanshu Kumar <himanshu064@gmail.com>
2024-09-04 17:22:24 +0530

    Working on the homepage design

--- src/components/@preline/Hero/HeroSection1.tsx
+++ src/components/@preline/Hero/HeroSection1.tsx
@@ -9,7 +9,7 @@ const HeroSection1 = () => {
             className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 focus:outline-none focus:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-200 dark:hover:border-neutral-600 dark:focus:border-neutral-600"
             href="#"
           >
-            PRO release - Join to waitlist
+            Hire Me
             <span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
               <svg
                 className="shrink-0 size-4"
@@ -28,19 +28,20 @@ const HeroSection1 = () => {
             </span>
           </a>
         </div>
-        <div className="mt-5 max-w-2xl text-center mx-auto">
+        <div className="mt-5 max-w-3xl text-center mx-auto">
           <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
-            Let&apos;s Build
+            Example using{" "}
             <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-              Together
+              NextAuth
             </span>
           </h1>
         </div>
 
         <div className="mt-5 max-w-3xl text-center mx-auto">
           <p className="text-lg text-gray-600 dark:text-neutral-400">
-            Preline UI is an open-source set of prebuilt UI components, ready-to-use examples and
-            Figma design system based on the utility-first Tailwind CSS framework.
+            This project shows how to integrate NextAuth with a Node.js Backend which is built with
+            express and MongoDB. The backend uses JWT for authentication and authorization. The
+            frontend is built with Next.js, Preline and Tailwind CSS.
           </p>
         </div>
 
@@ -49,7 +50,7 @@ const HeroSection1 = () => {
             className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:from-violet-600 focus:to-blue-600 py-3 px-4"
             href="#"
           >
-            Get started
+            View Demo
             <svg
               className="shrink-0 size-4"
               xmlns="http://www.w3.org/2000/svg"
@@ -69,7 +70,7 @@ const HeroSection1 = () => {
             type="button"
             className="relative group p-2 ps-3 inline-flex items-center gap-x-2 text-sm font-mono rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
           >
-            $ npm i preline
+            Source Code
             <span className="flex justify-center items-center bg-gray-200 rounded-md size-7 dark:bg-neutral-700 dark:text-neutral-400">
               <svg
                 className="shrink-0 size-4 group-hover:rotate-6 transition"
@@ -88,42 +89,6 @@ const HeroSection1 = () => {
               </svg>
             </span>
           </button>
-        </div>
-
-        <div className="mt-5 flex justify-center items-center gap-x-1 sm:gap-x-3">
-          <span className="text-sm text-gray-600 dark:text-neutral-400">Package Manager:</span>
-          <span className="text-sm font-bold text-gray-900 dark:text-white">npm</span>
-          <svg
-            className="size-5 text-gray-300 dark:text-neutral-600"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path d="M6 13L10 3" stroke="currentColor" stroke-linecap="round" />
-          </svg>
-          <a
-            className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
-            href="#"
-          >
-            Installation Guide
-            <svg
-              className="shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </a>
         </div>
       </div>
     </div>
--- src/components/@preline/Navbar/index.tsx
+++ src/components/@preline/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -10,7 +11,7 @@ const Navbar = () => {
             href="#"
             aria-label="Brand"
           >
-            Brand
+            Next Auth
           </a>
 
           {/* <!-- Collapse Button --> */}
@@ -68,247 +69,24 @@ const Navbar = () => {
           <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
             <div className="py-2 md:py-0  flex flex-col md:flex-row md:items-center gap-0.5 md:gap-1">
               <div className="grow">
-                <div className="flex flex-col md:flex-row md:justify-end md:items-center gap-0.5 md:gap-1">
-                  <a
-                    className="p-2 flex items-center text-sm bg-gray-100 text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-neutral-700 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                    href="#"
-                    aria-current="page"
-                  >
-                    <svg
-                      className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                      <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                    </svg>
-                    Landing
-                  </a>
-
-                  {/* <!-- Dropdown --> */}
-                  <div className="hs-dropdown [--strategy:static] md:[--strategy:fixed] [--adaptive:none] [--is-collapse:true] md:[--is-collapse:false] ">
-                    <button
-                      id="hs-header-base-dropdown"
-                      type="button"
-                      className="hs-dropdown-toggle w-full p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                      aria-haspopup="menu"
-                      aria-expanded="false"
-                      aria-label="Dropdown"
-                    >
-                      <svg
-                        className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <path d="m3 10 2.5-2.5L3 5" />
-                        <path d="m3 19 2.5-2.5L3 14" />
-                        <path d="M10 6h11" />
-                        <path d="M10 12h11" />
-                        <path d="M10 18h11" />
-                      </svg>
-                      Dropdown
-                      <svg
-                        className="hs-dropdown-open:-rotate-180 md:hs-dropdown-open:rotate-0 duration-300 shrink-0 size-4 ms-auto md:ms-1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <path d="m6 9 6 6 6-6" />
-                      </svg>
-                    </button>
-
-                    <div
-                      className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 relative w-full md:w-52 hidden z-10 top-full ps-7 md:ps-0 md:bg-white md:rounded-lg md:shadow-md before:absolute before:-top-4 before:start-0 before:w-full before:h-5 md:after:hidden after:absolute after:top-1 after:start-[18px] after:w-0.5 after:h-[calc(100%-0.25rem)] after:bg-gray-100 dark:md:bg-neutral-800 dark:after:bg-neutral-700"
-                      role="menu"
-                      aria-orientation="vertical"
-                      aria-labelledby="hs-header-base-dropdown"
-                    >
-                      <div className="py-1 md:px-1 space-y-0.5">
-                        <a
-                          className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                          href="#"
-                        >
-                          About
-                        </a>
-
-                        <div className="hs-dropdown [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover] [--is-collapse:true] md:[--is-collapse:false] relative">
-                          <button
-                            id="hs-header-base-dropdown-sub"
-                            type="button"
-                            className="hs-dropdown-toggle w-full flex justify-between items-center text-sm text-gray-800 rounded-lg p-2 md:px-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                          >
-                            Sub Menu
-                            <svg
-                              className="hs-dropdown-open:-rotate-180 md:hs-dropdown-open:-rotate-90 md:-rotate-90 duration-300 ms-auto shrink-0 size-4"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              stroke-width="2"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path d="m6 9 6 6 6-6" />
-                            </svg>
-                          </button>
-
-                          <div
-                            className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 relative md:w-48 hidden z-10 md:mt-2 md:!mx-[10px] md:top-0 md:end-full ps-7 md:ps-0 md:bg-white md:rounded-lg md:shadow-md dark:bg-neutral-800 dark:divide-neutral-700 before:hidden md:before:block before:absolute before:-end-5 before:top-0 before:h-full before:w-5 md:after:hidden after:absolute after:top-1 after:start-[18px] after:w-0.5 after:h-[calc(100%-0.25rem)] after:bg-gray-100 dark:md:bg-neutral-800 dark:after:bg-neutral-700"
-                            role="menu"
-                            aria-orientation="vertical"
-                            aria-labelledby="hs-header-base-dropdown-sub"
-                          >
-                            <div className="p-1 space-y-1">
-                              <a
-                                className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                                href="#"
-                              >
-                                About
-                              </a>
-
-                              <a
-                                className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                                href="#"
-                              >
-                                Downloads
-                              </a>
-
-                              <a
-                                className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                                href="#"
-                              >
-                                Team Account
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-
-                        <a
-                          className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                          href="#"
-                        >
-                          Downloads
-                        </a>
-
-                        <a
-                          className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                          href="#"
-                        >
-                          Team Account
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  {/* <!-- End Dropdown --> */}
-
-                  <a
-                    className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                      <circle cx="12" cy="7" r="4" />
-                    </svg>
-                    Account
-                  </a>
-
-                  <a
-                    className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M12 12h.01" />
-                      <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
-                      <path d="M22 13a18.15 18.15 0 0 1-20 0" />
-                      <rect width="20" height="14" x="2" y="6" rx="2" />
-                    </svg>
-                    Work
-                  </a>
-
-                  <a
-                    className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
-                      <path d="M18 14h-8" />
-                      <path d="M15 18h-5" />
-                      <path d="M10 6h8v4h-8V6Z" />
-                    </svg>
-                    Blog
-                  </a>
-                </div>
+                {/* Spacer */}
+                <div className="flex flex-col md:flex-row md:justify-end md:items-center gap-0.5 md:gap-1" />
               </div>
 
               {/* <!-- Button Group --> */}
               <div className="flex flex-wrap items-center gap-x-1.5">
-                <a
+                <Link
+                  href="/login"
                   className="py-[7px] px-2.5 inline-flex items-center font-medium text-sm rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 focus:outline-none focus:bg-gray-100 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                  href="#"
                 >
-                  Sign in
-                </a>
-                <a
+                  Login
+                </Link>
+                <Link
                   className="py-2 px-2.5 inline-flex items-center font-medium text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:bg-blue-600"
-                  href="#"
+                  href="/register"
                 >
                   Get started
-                </a>
+                </Link>
               </div>
             </div>
           </div>
